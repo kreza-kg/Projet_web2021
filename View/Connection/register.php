1efb12646c79786dd6c79a0b171c36d73bd884ad
maxence <guilbotmax@hotmail.fr>
2021-11-12 09:33:41 +0100

    Connexion / deconnexion + changement dynamique

--- View/Connection/register.php
+++ View/Connection/register.php
@@ -1,4 +1,29 @@
 <?php
+session_start();
+include('../../Config/Database.php');
+$connexion = connexionBd();
+
+if((isset($_POST['Nom']) && !empty($_POST['Nom'])) && (isset($_POST['Prenom']) && !empty($_POST['Prenom'])) && (isset($_POST['Pseudo']) && !empty($_POST['Pseudo'])) &&
+(isset($_POST['Mail']) && !empty($_POST['Mail'])) && (isset($_POST['Password']) && !empty($_POST['Password'])))
+{
+    $Nom = htmlspecialchars($_POST['Nom']);
+    $Prenom = htmlspecialchars($_POST['Prenom']);
+    $Pseudo = htmlspecialchars($_POST['Pseudo']);
+    $Mail = htmlspecialchars($_POST['Mail']);
+    $Password = htmlspecialchars($_POST['Password']);
+
+    $sql = $connexion->prepare("INSERT INTO compte ( Pseudo, nom, prenom, mail, password, id ) VALUES (:Nom, :Prenom,:Pseudo,:Mail,:Password, null)");
+    $sql->bindParam(':Nom', $Nom);
+    $sql->bindParam(':Prenom', $Prenom);
+    $sql->bindParam(':Pseudo', $Pseudo);
+    $sql->bindParam(':Mail', $Mail);
+    $sql->bindParam(':Password', $Password);
+
+    $sql->execute();
+    header('Location: ../../index.php');
+    $_SESSION['Pseudo'] = $Pseudo;
+
+}
 ?>
 <!doctype html>
 <html lang="fr">
@@ -10,6 +35,25 @@
     <title>Enregistrement</title>
 </head>
 <body>
+<div>
+    <form action="register.php" method="post">
+        <label><b>Nom</b></label>
+        <input type="text" placeholder="nom" name="Nom" required>
 
+        <label><b>Prenom</b></label>
+        <input type="text" placeholder="Prenom" name="Prenom" required>
+
+        <label><b>Nom d'utilisateur</b></label>
+        <input type="text" placeholder="Pseudo" name="Pseudo" required>
+
+        <label><b>Adresse email</b></label>
+        <input type="email" placeholder="Mail" name="Mail" required>
+
+        <label><b>Mot de passe</b></label>
+        <input type="password" placeholder="mot de passe" name="Password" required>
+
+        <input type="submit" value="M'inscrire"/>
+    </form>
+</div>
 </body>
 </html>
